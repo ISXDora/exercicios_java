5432bacfbe31594f28908371b127b524d28053c5
ISXDora <isinhasena@gmail.com>
2021-04-30 19:53:53 -0300

    Continuação dos exercícios de manipulação de strings e E/S de dados

---
+++ Documents/NetBeansProjects/exercicios_java/src/areaRetangulo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author isado
+ */
+public class areaRetangulo {
+    public static void main (String[]args){
+         int altura = 20 ;
+        int base = 30 ;
+        int area = altura * base ;
+        System.out.println("O valor da área do retângulo é "+ area+ "m².");
+    }
+}
---
+++ Documents/NetBeansProjects/exercicios_java/src/nomeCompleto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author isado
+ */
+public class nomeCompleto {
+    public static void main (String[]args){
+    String nome = "Isadora";
+    String sobrenome = "Xavier";
+        System.out.println("Meu nome é " + nome + " " + sobrenome);
+    }
+    
+}

ad8010725b2ac7ee11c11ec2f848bbee9ad4a48c
ISXDora <isinhasena@gmail.com>
2021-04-30 19:05:13 -0300

    Exercícios de manipulação de strings e e/s de dados

---
+++ Documents/NetBeansProjects/exercicios_java/src/imprimir/Imprimir.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imprimir;
+
+/**
+ *
+ * @author isado
+ */
+public class Imprimir {
+
+    public static void main(String[] args) {
+        System.out.println ("Bons estudos!");
+        System.out.println ("Bom dia usuário!");
+        System.out.println ("Seja bem vindo à mais uma vídeo aula!");
+        // TODO code application logic here
+    }
+    
+}

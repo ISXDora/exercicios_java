32b1e66fd051b91597b261fa8d14d3c1ba04fe0f
ISXDora <isinhasena@gmail.com>
2021-05-17 00:40:36 -0300

    jogo labirinto

---
+++ Documents/NetBeansProjects/exercicios_java/test/Tabuleiro2Test.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author isado
+ */
+public class Tabuleiro2Test {
+    
+    public Tabuleiro2Test() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of InicializarMatriz method, of class Tabuleiro2.
+     */
+    @Test
+    public void testInicializarMatriz() {
+        System.out.println("InicializarMatriz");
+        Tabuleiro2.InicializarMatriz();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of imprimir method, of class Tabuleiro2.
+     */
+    @Test
+    public void testImprimir() {
+        System.out.println("imprimir");
+        Tabuleiro2.imprimir();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class Tabuleiro2.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] Arg = null;
+        Tabuleiro2.main(Arg);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
